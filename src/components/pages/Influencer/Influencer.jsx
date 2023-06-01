ea6ce2b496e9d10643e7181157f67cce47afb9d3
Ehsan <ehsanellahiofficial@gmail.com>
2023-06-01 20:12:58 +0500

    Context Error Fixed

--- src/components/pages/Influencer/Influencer.jsx
+++ src/components/pages/Influencer/Influencer.jsx
@@ -3,20 +3,23 @@ import InfluencerCard from './InfluencerCard/InfluencerCard';
 import InfluencerNavData from './InfluencerNavbarContent/InfluencerNavData';
 import TabProvider from './../../../contexts/TabProvider';
 import InfluencerProvider from '../../../contexts/InfluencerProvider';
+import UserProvider from '../../../contexts/UserProvider';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 
 const Influencer = () => {
   const { id } = useParams();
   return (
-    <TabProvider>
-      <InfluencerProvider>
-        <section className={classes.influencerPage}>
-          <InfluencerCard influencerId={id} />
-          <InfluencerNavData />
-        </section>
-      </InfluencerProvider>
-    </TabProvider>
+    <UserProvider>
+      <TabProvider>
+        <InfluencerProvider>
+          <section className={classes.influencerPage}>
+            <InfluencerCard influencerId={id} />
+            <InfluencerNavData />
+          </section>
+        </InfluencerProvider>
+      </TabProvider>
+    </UserProvider>
   );
 };
 
